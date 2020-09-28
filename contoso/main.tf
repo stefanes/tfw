9d08cc51ff1c40e67f010e7964c0b12e381069e3
Stefan Eskelid <stefan.eskelid@aveva.com>
2020-09-28 12:42:35 +0000

    Refactor main.tf to use locals

--- contoso/main.tf
+++ contoso/main.tf
@@ -1,13 +1,24 @@
+locals {
+  prefix = "contoso"
+  region = "UK South"
+  tags = {
+    cost_center = "contoso research"
+  }
+}
+
 provider "azurerm" {
-    version = "~>2.13.0"
-    features {}    
+  version = "~>2.13.0"
+  features {}
 }
 
 resource "azurerm_resource_group" "contoso_rg" {
-    name = "contoso_rg"
-    location = "UK South"
+  name     = "${local.prefix}_rg"
+  location = local.region
+  tags     = local.tags
+}
 
-    tags = {
-        "cost_center" = "contoso research"
-    }
+resource "azurerm_resource_group" "contoso_dev_rg" {
+  name     = "${local.prefix}_dev_rg"
+  location = local.region
+  tags     = local.tags
 }
