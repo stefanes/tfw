20c315a50cb763cf02ccc22fb5b3a95c3f52595b
Stefan Eskelid <stefan.eskelid@aveva.com>
2020-09-29 11:37:28 +0000

    Use Azure as backend

--- contoso/main.tf
+++ contoso/main.tf
@@ -1,12 +1,21 @@
 # main.tf
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "tfstate"
+    # storage_account_name  = "tfstate1339"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   version = "~>2.13.0"
   features {}
 }
 
 module "connectedrg" {
-  # or remote git repo with ?ref=verion    
-  source = "./modules/contoso-az-connectedrg"
+  # source = "./modules/contoso-az-connectedrg"
+  source = "git@github.com:stefanes/tfw-modules.git//contoso-az-connectedrg?ref=v1.0"
 
   rg_names = var.rg_names
   vnets    = var.vnets
